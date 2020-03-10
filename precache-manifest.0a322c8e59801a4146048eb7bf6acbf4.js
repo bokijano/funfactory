9a5909d37f83407b193da223b50afca86e780dd5
Bojan Krstic <krstic.bojan80@gmail.com>
2020-03-10 22:06:39 +0100

    Updates

---
+++ precache-manifest.0a322c8e59801a4146048eb7bf6acbf4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e143960c2aa0c4a90e4",
+    "url": "/funfactory/static/css/main.45acd097.chunk.css"
+  },
+  {
+    "revision": "8e143960c2aa0c4a90e4",
+    "url": "/funfactory/static/js/main.8e143960.chunk.js"
+  },
+  {
+    "revision": "9467019640c34757fc1d",
+    "url": "/funfactory/static/js/runtime~main.94670196.js"
+  },
+  {
+    "revision": "919a613ff267c773a827",
+    "url": "/funfactory/static/js/2.919a613f.chunk.js"
+  },
+  {
+    "revision": "6b632eb18bbd5b1638ca23b7312a9c69",
+    "url": "/funfactory/static/media/cloud2.6b632eb1.png"
+  },
+  {
+    "revision": "9bc856b8c98b5e9d520139d1957fe539",
+    "url": "/funfactory/static/media/cloud.9bc856b8.png"
+  },
+  {
+    "revision": "48e2daf2b08490ee6805af500996d2a0",
+    "url": "/funfactory/static/media/cloud3.48e2daf2.png"
+  },
+  {
+    "revision": "d0265401ccd5ee77d74feb2de13d8216",
+    "url": "/funfactory/static/media/music2.d0265401.jpg"
+  },
+  {
+    "revision": "8cac17250583c439aa1bd4a4c2a78013",
+    "url": "/funfactory/static/media/music3.8cac1725.jpg"
+  },
+  {
+    "revision": "79a8c7501e1fd56e8b14268a6d721c98",
+    "url": "/funfactory/index.html"
+  }
+];
