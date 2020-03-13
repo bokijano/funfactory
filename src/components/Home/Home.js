9f75817204eb4dc330da49bf6cfb89ad74e870fd
Bojan Krstic <krstic.bojan80@gmail.com>
2020-03-13 23:31:06 +0100

    home page commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,56 +1,24 @@
 import React from "react";
 import "./Home.css";
-import Cloud1 from "./pictures/cloud.png";
-import Cloud2 from "./pictures/cloud2.png";
-import Cloud3 from "./pictures/cloud3.png";
 
-import Music1 from "./pictures/music/music2.jpg";
-import Music2 from "./pictures/music/music3.jpg";
+import Music from "./Music";
+import Movies from "./Movies";
+import Games from "./Games";
 
 function Home() {
   return (
     <React.Fragment>
-      <article className="container-cloud">
-        <section className="cloud1">
-          <img src={Cloud2} alt="second cloud" />
-        </section>
-        <section className="cloud2">
-          <img src={Cloud1} alt="first cloud" />
-        </section>
-        <section className="cloud3">
-          <img src={Cloud2} alt="second cloud" />
-        </section>
-        <section>
-          <img src={Cloud1} alt="first cloud" />
-        </section>
-        <section className="cloud4">
-          <img src={Cloud3} alt="third cloud" />
-        </section>
-        <section>
-          <img src={Cloud2} alt="second cloud" />
-        </section>
-        <article className="home-style">
-          <section className="music">
-            <div className="circle circle1">
-              <img src={Music1} alt="band picture" />
-            </div>
-            <div className="circle circle2">
-              <img src={Music2} alt="band picture" />
-            </div>
-            <h1>Music</h1>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet,
-              nisi nemo distinctio fuga magnam repellat et molestiae natus illo
-              maxime quisquam veniam itaque deserunt optio? Obcaecati animi
-              ducimus assumenda, iusto pariatur veniam at quia doloremque minus
-              totam, consectetur labore libero qui earum reiciendis facilis.
-              Enim quam temporibus impedit laudantium? Iusto?
-            </p>
-          </section>
-          <section className="movies"></section>
-          <section className="games"></section>
-        </article>
-      </article>
+      <div className="home-display">
+        <div className="music-display">
+          <Music />
+        </div>
+        <div className="movies-display">
+          <Movies />
+        </div>
+        <div className="games-display">
+          <Games />
+        </div>
+      </div>
     </React.Fragment>
   );
 }
---
+++ src/components/Home/Movies.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+import Leaf from "./pictures/leaves/leaf.png";
+import Leaf1 from "./pictures/leaves/leaf1.png";
+import Leaf2 from "./pictures/leaves/leaf2.png";
+import Leaf3 from "./pictures/leaves/leaf3.png";
+import Leaf4 from "./pictures/leaves/leaf4.png";
+import Leaf5 from "./pictures/leaves/leaf5.png";
+import Leaf6 from "./pictures/leaves/leaf6.png";
+import Leaf7 from "./pictures/leaves/leaf7.png";
+
+export default function Movies() {
+  return (
+    <React.Fragment>
+      <article className="container-leaves">
+        <section className="back-leaves">
+          <div className="set-leaves">
+            <div>
+              <img src={Leaf} alte="leaf one" />
+            </div>
+            <div>
+              <img src={Leaf1} alt="leaf two" />
+            </div>
+            <div>
+              <img src={Leaf2} alt="leaf three" />
+            </div>
+            <div>
+              <img src={Leaf3} alt="leaf four" />
+            </div>
+            <div>
+              <img src={Leaf4} alt="leaf five" />
+            </div>
+            <div>
+              <img src={Leaf5} alt="leaf six" />
+            </div>
+            <div>
+              <img src={Leaf6} alt="leaf even" />
+            </div>
+            <div>
+              <img src={Leaf7} alt="leaf eight" />
+            </div>
+            <div>
+              <img src={Leaf5} alt="leaf eight" />
+            </div>
+            <div>
+              <img src={Leaf6} alt="leaf nine" />
+            </div>
+          </div>
+        </section>
+
+        <article className="movies-style">
+          <section className="movies">
+            <h1>
+              <span>m</span>
+              <span>o</span>
+              <span>v</span>
+              <span>i</span>
+              <span>e</span>
+              <span>s</span>
+            </h1>
+          </section>
+        </article>
+      </article>
+    </React.Fragment>
+  );
+}
